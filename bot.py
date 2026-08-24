5aa7dc5167fd07229183d39337f45097ad54921e
aa4onz <aa4onz@gmail.com>
2026-08-24 19:49:13 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -628,6 +628,7 @@ async def text_alt(ctx, main_user: discord.Member, alt_user: discord.Member):
     await alt_user.add_roles(role)
     
     msg = f"alt linked"
+    await interaction.response.send_message(msg)
     
 
 
@@ -808,6 +809,7 @@ async def slash_alt(interaction: discord.Interaction, main_user: discord.Member,
     await alt_user.add_roles(role)
     
     msg = f"alt linked"
+    await interaction.response.send_message(msg)
     
 
 
