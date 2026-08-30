958add0bf48fbdbcfccf2fe9af414cb6ab95ba1f
aa4onz <aa4onz@gmail.com>
2026-08-30 12:31:15 -0400

    Update topruns.py

--- topruns.py
+++ topruns.py
@@ -5,5 +5,6 @@ import menus
 async def exec(tgt):
     chan, _, resp = ctx_parse(tgt)
     if str(chan.id) in ch: return
-    v = menus.create_paginator(list(db_top.find().sort("pace", -1).limit(100)), "Top Fastest Runs", d.Color.purple())
+    uid = tgt.author.id if isinstance(tgt, c.Context) else tgt.user.id
+    v = menus.create_paginator(list(db_top.find().sort("pace", -1).limit(100)), "Top Fastest Runs", d.Color.purple(), is_lb=False, author_id=uid)
     await (resp.send(embed=v.build_embed(), view=v) if isinstance(tgt, c.Context) else resp.send_message(embed=v.build_embed(), view=v))
