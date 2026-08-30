99126b1d81016d46f2b0afe7715495dbed147c66
aa4onz <aa4onz@gmail.com>
2026-08-30 19:22:30 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,9 +10,21 @@ def deadline():
         db_sys.insert_one(r)
     return r
 
+def get_main(uid):
+    uid_str = str(uid)
+    # Check fast cache first
+    if uid_str in alts:
+        return alts[uid_str]
+    # Direct database lookup safety net fallback
+    doc = db_alt.find_one({"_id": uid_str})
+    if doc:
+        main_id = str(doc.get("main_id"))
+        alts[uid_str] = main_id  # Save to memory cache for next count speed
+        return main_id
+    return uid_str
+
 def score_up(uid):
-    db_lb.update_one({"_id": alts.get(str(uid), str(uid))}, {"$inc": {"correct_counts": 1}}, upsert=True)
+    db_lb.update_one({"_id": get_main(uid)}, {"$inc": {"correct_counts": 1}}, upsert=True)
 
 def race_up(cid, uid):
-    db_rc.update_one({"_id": f"race_{cid}"}, {"$inc": {f"players.{alts.get(str(uid), str(uid))}": 1, "total_counts": 1
-                                                      }})
+    db_rc.update_one({"_id": f"race_{cid}"}, {"$inc": {f"players.{get_main(uid)}": 1, "total_counts": 1}})
