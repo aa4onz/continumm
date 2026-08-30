fc16e42e5e2134233b125690f30226de9e5597bc
aa4onz <aa4onz@gmail.com>
2026-08-30 09:30:41 +0600

    Fix variable names in exec function

--- topruns.py
+++ topruns.py
@@ -3,8 +3,7 @@ from utils import ctx_parse
 import menus
 
 async def exec(tgt):
-    ch, _, resp = ctx_parse(tgt)
-    if str(ch.id) in ch: return
+    chan, _, resp = ctx_parse(tgt)
+    if str(chan.id) in ch: return
     v = menus.create_paginator(list(db_top.find().sort("pace", -1).limit(100)), "Top Fastest Runs", d.Color.purple())
     await (resp.send(embed=v.build_embed(), view=v) if isinstance(tgt, c.Context) else resp.send_message(embed=v.build_embed(), view=v))
-  
