e55f33f74b48c27909665c42a6c035b1e231e3e1
aa4onz <aa4onz@gmail.com>
2026-08-30 09:12:11 +0600

    Refactor lock and unlock commands to async functions

--- prefix.py
+++ prefix.py
@@ -7,8 +7,11 @@ def register():
     bot.add_command(c.Command(pace.exec, name='pace'))
     bot.add_command(c.Command(topruns.exec, name='topruns'))
     bot.add_command(c.Command(lb.exec, name='lb'))
-    bot.add_command(c.Command(lambda ctx: lock.exec(ctx, True), name='lock'))
-    bot.add_command(c.Command(lambda ctx: lock.exec(ctx, False), name='unlock'))
     bot.add_command(c.Command(alt.exec, name='alt'))
     bot.add_command(c.Command(reset.exec, name='reset'))
-  
+    async def cmd_lock(ctx): 
+        await lock.exec(ctx, True)
+    async def cmd_unlock(ctx): 
+        await lock.exec(ctx, False)
+    bot.add_command(c.Command(cmd_lock, name='lock'))
+    bot.add_command(c.Command(cmd_unlock, name='unlock'))
