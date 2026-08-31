24726e36d2fbe568e407620bb4fc4de188cc9ca9
aa4onz <aa4onz@gmail.com>
2026-08-31 15:16:27 -0400

    Update slash.py

--- slash.py
+++ slash.py
@@ -1,35 +1,41 @@
 from variables import *
+from discord import app_commands
 import run, end, pace, topruns, lb, lock, alt, reset, cn
 
 def register():
-    @bot.tree.command(name='run')
+    # --- PUBLIC COMMANDS ---
+    @bot.tree.command(name='run', description='start race')
     async def s_run(i): await run.exec(i)
     
-    @bot.tree.command(name='end')
+    @bot.tree.command(name='end', description='end the active race and calculate the final pace')
     async def s_end(i): await end.exec(i)
     
-    @bot.tree.command(name='pace')
+    @bot.tree.command(name='pace', description='check the current pace')
     async def s_pace(i): await pace.exec(i)
     
-    @bot.tree.command(name='topruns')
+    @bot.tree.command(name='topruns', description='view the leaderboard of the top 100 fastest races')
     async def s_top(i): await topruns.exec(i)
     
-    @bot.tree.command(name='lb')
+    @bot.tree.command(name='lb', description='view the 14day server leaderboard')
     async def s_lb(i): await lb.exec(i)
     
-    @bot.tree.command(name='lock')
+    # --- ADMIN LOCKED COMMANDS ---
+    @bot.tree.command(name='lock', description='lock the counting channel')
+    @app_commands.checks.has_permissions(administrator=True)
     async def s_lock(i): await lock.exec(i, True)
     
-    @bot.tree.command(name='unlock')
+    @bot.tree.command(name='unlock', description='unlock the counting channel')
+    @app_commands.checks.has_permissions(administrator=True)
     async def s_unlock(i): await lock.exec(i, False)
     
-    @bot.tree.command(name='reset')
+    @bot.tree.command(name='reset', description='reset the 14-day scoreboard and dates')
+    @app_commands.checks.has_permissions(administrator=True)
     async def s_reset(i): await reset.exec(i)
     
-    @bot.tree.command(name='alt')
+    @bot.tree.command(name='alt', description='link a secondary alternate account to a primary main user profile')
+    @app_commands.checks.has_permissions(administrator=True)
     async def s_alt(i, m: d.Member, a: d.Member): await alt.exec(i, m, a)
     
-    @bot.tree.command(name='cn', description='Set a fallback username for a member who left')
+    @bot.tree.command(name='cn', description='set a custom fallback text username for members who left the server')
+    @app_commands.checks.has_permissions(administrator=True)
     async def s_cn(i, user_id: str, custom_name: str): await cn.exec(i, user_id, custom_name=custom_name)
-
-      
