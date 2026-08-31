820377ff6a1635873cd0e17041919863067899b9
aa4onz <aa4onz@gmail.com>
2026-08-31 15:17:02 -0400

    Add admin checks for alt, reset, cn, lock, and unlock commands
    Refactor command registration to add admin checks for specific commands.

--- prefix.py
+++ prefix.py
@@ -1,4 +1,5 @@
 from variables import *
+from discord.ext import commands
 import run, end, pace, topruns, lb, lock, alt, reset, cn
 
 def register():
@@ -7,12 +8,28 @@ def register():
     bot.add_command(c.Command(pace.exec, name='pace'))
     bot.add_command(c.Command(topruns.exec, name='topruns'))
     bot.add_command(c.Command(lb.exec, name='lb'))
-    bot.add_command(c.Command(alt.exec, name='alt'))
-    bot.add_command(c.Command(reset.exec, name='reset'))
+    
+    # --- ADMIN LOCKED TEXT COMMANDS ---
+    cmd_alt = c.Command(alt.exec, name='alt')
+    cmd_alt.add_check(commands.has_permissions(administrator=True).predicate)
+    bot.add_command(cmd_alt)
+    
+    cmd_reset = c.Command(reset.exec, name='reset')
+    cmd_reset.add_check(commands.has_permissions(administrator=True).predicate)
+    bot.add_command(cmd_reset)
+    
+    cmd_cn = c.Command(cn.exec, name='cn')
+    cmd_cn.add_check(commands.has_permissions(administrator=True).predicate)
+    bot.add_command(cmd_cn)
+    
     async def cmd_lock(ctx): 
         await lock.exec(ctx, True)
+    c_lock = c.Command(cmd_lock, name='lock')
+    c_lock.add_check(commands.has_permissions(administrator=True).predicate)
+    bot.add_command(c_lock)
+    
     async def cmd_unlock(ctx): 
         await lock.exec(ctx, False)
-    bot.add_command(c.Command(cmd_lock, name='lock'))
-    bot.add_command(c.Command(cmd_unlock, name='unlock'))
-    bot.add_command(c.Command(cn.exec, name='cn'))
+    c_unlock = c.Command(cmd_unlock, name='unlock')
+    c_unlock.add_check(commands.has_permissions(administrator=True).predicate)
+    bot.add_command(c_unlock)
