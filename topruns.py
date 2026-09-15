29b6aeec3368e78a00c136badd137971bcf1df3a
aa4onz <aa4onz@gmail.com>
2026-09-14 20:08:53 -0400

    Refactor exec function for better readability

--- topruns.py
+++ topruns.py
@@ -4,7 +4,18 @@ import menus
 
 async def exec(tgt):
     chan, _, resp = ctx_parse(tgt)
-    if str(chan.id) in ch: return
+    if str(chan.id) in ch: 
+        return
+        
     uid = tgt.author.id if isinstance(tgt, c.Context) else tgt.user.id
-    v = menus.create_paginator(list(db_top.find().sort("pace", -1).limit(100)), "Top Fastest Runs", d.Color.purple(), is_lb=False, author_id=uid)
+    
+    # Pulls entries sorted from highest new precision pace down to lowest
+    v = menus.create_paginator(
+        list(db_top.find().sort("pace", -1).limit(100)), 
+        "Top Fastest Runs", 
+        d.Color.purple(), 
+        is_lb=False, 
+        author_id=uid
+    )
+    
     await (resp.send(embed=v.build_embed(), view=v) if isinstance(tgt, c.Context) else resp.send_message(embed=v.build_embed(), view=v))
